04854f5bf3978650c374c026202173168608e31d
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-14 10:41:28 -0700

    Create basic components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   const authorSlug = "bar"
   return (
     <>
+    {/* delete there codes */}
       <h1>Twootr</h1>
       <img src={`https://avatars.dicebear.com/api/bottts/${authorSlug}.svg`} />
     </>
