7f9b90503ef12d4d14dec7a15ee2aac74de63f28
michiru6828 <since_l999@yahoo.co.jp>
2023-07-17 09:25:36 -0700

    combined components

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,42 +1,26 @@
-import React from 'react'
-import Post from './Post'
-import Loader from './Loader'
-import { useState, useEffect } from 'react';
-import styled from 'styled-components'
-import { axiosInstance } from "../axios";
+// import React from "react";
+import Post from "./Post";
+import Loader from "./Loader";
+import styled from "styled-components";
 
 const PostListDiv = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 30px;
-`
+`;
 
-export default function PostList() {
-
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axiosInstance.get("/twoots")
-      setPosts(response.data)
-      setTimeout(() => {
-        setLoading(false)
-      }, 1500)
-    }
-
-    fetchData()    
-  }, [])
-  
+export default function PostList({ posts, isLoading }) {
   return (
     <>
-      { isLoading ? <Loader/> : (
+      {isLoading ? (
+        <Loader />
+      ) : (
         <PostListDiv>
           {posts.map((post) => {
-            return <Post key={post._id} post={post} />
+            return <Post key={post._id} post={post} />;
           })}
         </PostListDiv>
       )}
     </>
-  )
+  );
 }
